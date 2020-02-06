3b3d0f3161c1ac62017c29449184db1b044b676d
DBelda <davidbelda00@gmail.com>
2020-02-06 12:37:17 +0100

    he introducido varios system outs
    Signed-off-by: DBelda <davidbelda00@gmail.com>

--- projectoHola/src/projectoHola/proyectoHola.java
+++ projectoHola/src/projectoHola/proyectoHola.java
@@ -5,6 +5,10 @@ public class proyectoHola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		System.out.println("Hola a todos");
+		System.out.println("Hola universo");
+
+
 	}
 
 }
