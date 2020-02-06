a3e24500a9a022d017164d3ad4b857d33caa62d4
DBelda <davidbelda00@gmail.com>
2020-02-06 12:32:46 +0100

    Mi primera versión
    Comentarios sobre el Commit
    Signed-off-by: DBelda <davidbelda00@gmail.com>

---
+++ projectoHola/src/projectoHola/proyectoHola.java
@@ -0,0 +1,10 @@
+package projectoHola;
+
+public class proyectoHola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+
+}
